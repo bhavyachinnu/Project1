7b94939a97d277a5cdb192453e8376301c98edb3
Bhavya <bhavya.chinnu94@gmail.com>
2025-05-07 23:12:49 +0530

    Arch1

---
+++ src/test/java/Practice/Tests/Test2.java
@@ -0,0 +1,13 @@
+package Practice.Tests;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+
+	@Test
+	public void test()
+	{
+		System.out.println("1 from Test2");
+		System.out.println("2 from Test2");
+	}
+}
