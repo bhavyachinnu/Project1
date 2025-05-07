c95335a94f2af0695eae8c3fe4533346c4d7f798
Bhavya <bhavya.chinnu94@gmail.com>
2025-05-07 23:22:16 +0530

    Project1 Archt2

--- src/test/java/Practice/Tests/Test2.java
+++ src/test/java/Practice/Tests/Test2.java
@@ -10,4 +10,11 @@ public class Test2 {
 		System.out.println("1 from Test2");
 		System.out.println("2 from Test2");
 	}
+	
+	@Test
+	public void test1()
+	{
+		System.out.println("1 from Test2 from project1");
+		System.out.println("2 from Test2 from project1");
+	}
 }
